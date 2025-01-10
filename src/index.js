aea67093610fb562145869c3d468bf5a8fa5f65a
RhexNiebres <rhexniebres@gmail.com>
2025-01-10 10:23:16 -0800

    add method get, has and remove

--- src/index.js
+++ src/index.js
@@ -1,18 +1,65 @@
 class HashMap{
-    constructor(loadfactor, capacity = 10){
+    constructor(loadfactor, capacity = 16){
         this.loadfactor = loadfactor;
         this.capacity = capacity;
+        this.size = 0;
+        this.buckets = new Array(this.capacity);
     }
 
     hash(key) {
         let hashCode = 0;
-        const primeNumber = 31;
-    
+        const primeNumber = 31; //for better distribution to avoid collisions
+
         for (let i = 0; i < key.length; i++) {
-          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;//% to ensure output do not become larger than the buckets length  
         }
-    
+
         return hashCode;
-      }
-      
+    }
+ 
+    get(key){
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        if (bucket) {
+            for (let i = 0; i < bucket.length; i++){
+                if (bucket[i].key === key){
+                    return bucket[i].value;
+                }
+            }
+        }
+        return null;
+    }
+
+    has(key){
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        if (bucket) {
+            for (let i = 0; i < bucket.length; i++){
+                if (bucket[i].key === key){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    remove(key){
+        const index = this.hash(key);
+        
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+        const bucket = this.buckets[index];
+
+        if (bucket) {
+            for (let i = 0; i < bucket.length; i++){
+                if (bucket[i].key === key){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
